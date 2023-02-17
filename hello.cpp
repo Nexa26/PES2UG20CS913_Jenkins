8c507dbe0a334f362c5d24b9c52e14c13a78aa65
Neha Paramatmuni <n05e02xa@gmail.com>
2023-02-17 09:35:32 +0530

    task2

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout<<"Hello,PES2UG20CS913!\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
